1fc0343a840258cb4485c92574c7841aa20bd727
vittalpandu07 <vittalpandu07@gmail.com>
2025-09-15 01:06:14 +0530

    tags updated

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket        = "my-bucket-2025-new2"
+  bucket        = "my-bucket-2025-new3"
   force_destroy = true
+  region = "eu-north-1"
   tags = {
     Environment = "prod"
     Name        = "My bucket1"
