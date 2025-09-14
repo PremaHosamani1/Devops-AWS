17a90924d64460832b177766d7c64df93b5b3c26
vittalpandu07 <vittalpandu07@gmail.com>
2025-09-15 02:03:14 +0530

    Format Terraform files

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket        = "my-bucket-2025-new3"
+  bucket        = "my-bucket-2025-new4"
   force_destroy = true
   region        = "us-east-1"
   tags = {
     Environment = "prod"
-    Name        = "My bucket1"
+    Name        = "My bucket"
   }
 
 }
