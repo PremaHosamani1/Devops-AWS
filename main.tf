ec8b355cd02913763282be71303296d154e45a50
vittalpandu07 <vittalpandu07@gmail.com>
2025-09-15 01:14:18 +0530

    tags updated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket        = "my-bucket-2025-new3"
   force_destroy = true
-  region = "eu-north-1"
+  region = "us-east-q"
   tags = {
     Environment = "prod"
     Name        = "My bucket1"
--- provider.tf
+++ provider.tf
@@ -11,5 +11,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
